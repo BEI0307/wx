f7fc1abfbd8fcb19868ed391255c15acb82b3f24
BEI0307 <115714398+BEI0307@users.noreply.github.com>
2022-10-21 15:44:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '困','BEI',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oeGip6CwLVEumMnaCu_M_RzyhqRs',
+      id: 'oeGip6CwLVEumMnaCu_M_RzyhqRs','oeGip6LoIAFHw0_0Ven0cRNdo5dk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'gWnYn5dMtt4qGb9ChNN6Iqn70AXiYo9CI902aTnDWG8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
